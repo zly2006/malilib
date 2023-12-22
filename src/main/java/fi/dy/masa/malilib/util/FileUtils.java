aaff6cf54dda85fa4a6d876fe113f389c7ce2321
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-12-22 13:41:05 -0500

    Update FileUtils.java

--- src/main/java/fi/dy/masa/malilib/util/FileUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/FileUtils.java
@@ -14,7 +14,6 @@ import fi.dy.masa.malilib.MaLiLib;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtIo;
-//import net.minecraft.nbt.NbtTagSizeTracker;
 import net.minecraft.nbt.NbtSizeTracker;
 
 public class FileUtils {
