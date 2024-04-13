f2bbad1c3679a366c4255d234931b6d9e20a4cfb
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-04-12 21:03:50 -0400

    1.20.5-pre.1 -- more cleanups (Added PR #150 TODO)

--- src/main/java/fi/dy/masa/malilib/mixin/MixinMouse.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinMouse.java
@@ -6,9 +6,11 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.util.Window;
+
 import fi.dy.masa.malilib.event.InputEventHandler;
 
 @Mixin(Mouse.class)
