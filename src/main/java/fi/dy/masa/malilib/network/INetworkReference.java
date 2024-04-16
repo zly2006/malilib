e8d1e5293c9a28d573feac0b42e73718587e58fc
Sakura Ryoko <sakura@ryoko.soy>
2024-04-16 12:08:46 -0400

    1.20.5-pre.3 some cleanups

--- src/main/java/fi/dy/masa/malilib/network/INetworkReference.java
+++ src/main/java/fi/dy/masa/malilib/network/INetworkReference.java
@@ -1,5 +1,7 @@
 package fi.dy.masa.malilib.network;
 
+import fi.dy.masa.malilib.MaLiLibReference;
+
 import java.net.InetAddress;
 
 public interface INetworkReference
@@ -10,4 +12,7 @@ public interface INetworkReference
     void setLocalIpAddr(InetAddress addr);
     void setDedicated(boolean toggle);
     void setOpenToLan(boolean toggle);
+
+    default boolean isClient() { return MaLiLibReference.isClient(); }
+    default boolean isServer() { return MaLiLibReference.isServer(); }
 }
