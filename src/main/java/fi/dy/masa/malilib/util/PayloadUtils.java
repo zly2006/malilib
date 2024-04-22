887c0d5bd17e2b318681fbeefe9c0224e4eb959a
Sakura Ryoko <sakura@ryoko.soy>
2024-04-22 08:44:29 -0400

    Add helpers for Payload / Handler registration

--- src/main/java/fi/dy/masa/malilib/util/PayloadUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/PayloadUtils.java
@@ -8,11 +8,24 @@ import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtSizeTracker;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import fi.dy.masa.malilib.MaLiLib;
 import fi.dy.masa.malilib.network.MaLiLibBuf;
 
 public class PayloadUtils
 {
+    public final static int FROM_SERVER = 1;
+    public final static int TO_SERVER = 2;
+    public final static int BOTH_SERVER = 3;
+    public final static int TO_CLIENT = 4;
+    public final static int FROM_CLIENT = 5;
+    public final static int BOTH_CLIENT = 6;
+
     @Nullable
     public static PacketByteBuf toPacketByteBuf(@Nonnull MaLiLibBuf in)
     {
@@ -106,5 +119,38 @@ public class PayloadUtils
         }
 
         return null;
+    }
+
+    public static <T extends CustomPayload> void registerPlayPayload(int direction, @Nonnull CustomPayload.Id<T> id, @Nonnull PacketCodec<? super RegistryByteBuf,T> codec)
+    {
+        switch (direction)
+        {
+            case TO_SERVER, FROM_CLIENT -> PayloadTypeRegistry.playC2S().register(id, codec);
+            case FROM_SERVER, TO_CLIENT -> PayloadTypeRegistry.playS2C().register(id, codec);
+            default ->
+            {
+                PayloadTypeRegistry.playC2S().register(id, codec);
+                PayloadTypeRegistry.playS2C().register(id, codec);
+            }
+        }
+    }
+
+    public static <T extends CustomPayload> boolean registerPlayReceiver(@Nonnull CustomPayload.Id<T> id, @Nonnull ClientPlayNetworking.PlayPayloadHandler<T> receiver)
+    {
+        try
+        {
+            return ClientPlayNetworking.registerGlobalReceiver(id, receiver);
+        }
+        catch (IllegalArgumentException e)
+        {
+            MaLiLib.logger.error("registerPlayReceiver IllegalArgumentException: Payload not registered");
+        }
+
+        return false;
+    }
+
+    public static void unregisterPlayHandler(@Nonnull Identifier id)
+    {
+        ClientPlayNetworking.unregisterGlobalReceiver(id);
     }
 }
