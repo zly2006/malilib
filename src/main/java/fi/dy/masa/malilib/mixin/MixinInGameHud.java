6861afeb711a3863a75823f74598ef26fdc1eaba
Sakura Ryoko <sakura@ryoko.soy>
2024-09-18 08:05:42 -0400

    add PR#164 (masa's repo)

--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,8 +1,10 @@
 package fi.dy.masa.malilib.mixin;
 
+import net.minecraft.client.gui.LayeredDrawer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,9 +18,17 @@ import fi.dy.masa.malilib.event.RenderEventHandler;
 public abstract class MixinInGameHud
 {
     @Shadow @Final private MinecraftClient client;
+    @Shadow @Final private LayeredDrawer layeredDrawer;
 
-    @Inject(method = "render", at = @At("RETURN"))
-    private void onGameOverlayPost(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onInit(CallbackInfo info)
+    {
+        this.layeredDrawer.addLayer(this::malilib_onGameOverlayPost);
+    }
+
+    //@Inject(method = "render", at = @At("RETURN"))
+    @Unique
+    private void malilib_onGameOverlayPost(DrawContext context, RenderTickCounter tickCounter)
     {
         ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(context, this.client, tickCounter.getTickDelta(false));
     }
